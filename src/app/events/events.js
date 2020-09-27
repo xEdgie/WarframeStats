d208b4489cdbebfda029edaea748ed7fe5a33ebc
xEdgie <xEdgie@users.noreply.github.com>
2020-09-27 12:21:01 +0200

    Update

--- src/app/events/events.js
+++ src/app/events/events.js
@@ -15,6 +15,33 @@ function Start()
 	getCetusState();
 }
 
+var countDownDate = new Date(Date.UTC("Aug 1, 2020 12:00:00")).getTime();
+
+var x = setInterval(
+	function() 
+	{
+		//Get current time
+		var currentDate = new Date().getTime();
+	
+		var distance = countDownDate - currentDate;
+	
+		// Time calculations for days, hours, minutes and seconds
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	
+		// Display the result in the element with id="demo"
+		document.getElementById("tennocontime").innerHTML = days + "d " + hours + "h "
+		+ minutes + "m " + seconds + "s ";
+	
+		// If the count down is finished, write some text
+		if (distance < 0) {
+		clearInterval(x);
+		document.getElementById("tennocontime").innerHTML = "EXPIRED";
+		}
+	}, 1000);
+
 //Api Queries
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
--- src/app/framestats/framestats.js
+++ src/app/framestats/framestats.js
@@ -6,7 +6,7 @@ var Warframe;
 //Functions
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-function LowerCase(value)
+function LowerCase (value)
 {
 	return value.toString().toLowerCase();
 }
@@ -31,17 +31,76 @@ function getWarframeStats()
 			//Parse Data
 			var json = JSON.parse(this.responseText);
 
-            //Extract Data
+            //Basic Stats
 			document.getElementById("warframename").innerHTML = "Name : " + json[0].name;
 			document.getElementById("health").innerHTML = "Health : " + json[0].health;
 			document.getElementById("shield").innerHTML = "Shield : " + json[0].shield;
 			document.getElementById("armor").innerHTML = "Armor : " + json[0].armor;
 			document.getElementById("sprint").innerHTML = "Sprint Speed : " + json[0].sprintSpeed;
-
 			document.getElementById("description").innerHTML = "Description : " + json[0].description;
-
 			document.getElementById("mastery").innerHTML = "Mastery Requirement : " + json[0].masteryReq;
-			document.getElementById("Thumbnail").src = json[0].wikiaThumbnail;
+			document.getElementById("thumbnail").src = json[0].wikiaThumbnail;
+
+			//Abilities
+			document.getElementById("ability1").innerHTML = json[0].abilities[0].name;
+			document.getElementById("ability1Description").innerHTML = json[0].abilities[0].description;
+
+			document.getElementById("ability2").innerHTML = json[0].abilities[1].name;
+			document.getElementById("ability2Description").innerHTML = json[0].abilities[1].description;
+
+			document.getElementById("ability3").innerHTML = json[0].abilities[2].name;
+			document.getElementById("ability3Description").innerHTML = json[0].abilities[2].description;
+
+			document.getElementById("ability4").innerHTML = json[0].abilities[3].name;
+			document.getElementById("ability4Description").innerHTML = json[0].abilities[3].description;
+
+			//Components
+			var relic1 = "Relic : ";
+			var relic2 = "Relic : ";
+			var relic3 = "Relic : ";
+			var relic4 = "Relic : ";
+
+			if(json[0].components[1].tradable == true)
+			{
+				document.getElementById("component1").innerHTML = json[0].name + " " + json[0].components[1].name;
+			}
+			else
+			{
+				document.getElementById("component1").innerHTML = json[0].components[1].name;
+			}
+
+			if(json[0].components[2].tradable == true)
+			{
+				document.getElementById("component2").innerHTML = json[0].name + " " + json[0].components[2].name;
+			}
+			else
+			{
+				document.getElementById("component2").innerHTML = json[0].components[2].name;
+			}
+
+			if(json[0].components[3].tradable == true)
+			{
+				document.getElementById("component3").innerHTML = json[0].name + " " + json[0].components[3].name;
+			}
+			else
+			{
+				document.getElementById("component3").innerHTML = json[0].components[3].name;
+			}
+
+			if(json[0].components[4].tradable == true)
+			{
+				document.getElementById("component4").innerHTML = json[0].name + " " + json[0].components[4].name;
+			}
+			else
+			{
+				document.getElementById("component4").innerHTML = json[0].components[4].name;
+			}
+
+			//Relics
+			document.getElementById("relic1").innerHTML = "Relic : " + json[0].components[1].drops[0].location;
+			document.getElementById("relic2").innerHTML = "Relic : " + json[0].components[2].drops[0].location;
+			document.getElementById("relic3").innerHTML = "Relic : " + json[0].components[3].drops[0].location;
+			document.getElementById("relic4").innerHTML = "Relic : " + json[0].components[4].drops[0].location;
 		}
     }
 
